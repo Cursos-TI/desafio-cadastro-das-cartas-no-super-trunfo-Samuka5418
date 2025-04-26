f989ef18e1cb563dc9179c5f6486001fb2ca6b11
Samuka5418 <167463193+Samuka5418@users.noreply.github.com>
2025-04-26 22:33:01 +0000

    Fim

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main()
  {
     
     char estado1, estado2;
-    char codigo1[4], codigo2 [4];s
+    char codigo1[4], codigo2 [4];
     char cidade1 [20], cidade2 [20];
     unsigned int populacao1, populacao2;
     float area1, area2;
@@ -24,7 +24,7 @@ int main()
     scanf(" %3s", codigo1);
 
     printf("Digite o Nome da cidade:\n");
-    scanf(" %s", &cidade1);
+    scanf(" %19s", cidade1);
 
     printf("Digite a População:\n");
     scanf("%u", &populacao1);
@@ -46,11 +46,11 @@ int main()
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("População: %u\n", populacao1);
-    printf("Área: %f\n", area1);
-    printf("Pib: %f\n", pib1);
+    printf("Área: %.2f\n", area1);
+    printf("Pib: %.2f\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos1);
-    printf("Dencidade Populacional: %f\n", densidade1);
-    printf("Pib per Capital: %f\n\n", pibcapital1);
+    printf("Dencidade Populacional: %.2f\n", densidade1);
+    printf("Pib per Capital: %.2f\n\n", pibcapital1);
 
     // CARTA 2 ---------------------------------------------------------------
     printf("------------ Carta2 ------------\n\n");
@@ -61,12 +61,10 @@ int main()
     setbuf(stdin,NULL);
 
     printf("Digite a Letra do Estado, Seguida de um NÚmero de 01 a 04:\n");
-    scanf("%c", &codigo2);
-    
-    setbuf(stdin,NULL);
+    scanf(" %3s", codigo2);
 
     printf("Digite o Nome da cidade:\n");
-    scanf("%s", &cidade2);
+    scanf(" %19s", cidade2);
 
     printf("Digite a População:\n");
     scanf("%u", &populacao2);
@@ -88,11 +86,11 @@ int main()
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("População: %u\n", populacao2);
-    printf("Área: %f\n", area2);
-    printf("Pib: %f\n", pib2);
+    printf("Área: %.2f\n", area2);
+    printf("Pib: %.2f\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos2);
-    printf("Dencidade Populacional: %f\n", densidade2);
-    printf("Pib per Capital: %f\n", pibcapital2);
+    printf("Dencidade Populacional: %.2f\n", densidade2);
+    printf("Pib per Capital: %.2f\n", pibcapital2);
 
     float densidadeEcapital1;
     float densidadeEcapital2;
@@ -111,8 +109,20 @@ int main()
     int resupopulacao, resuarea, resupib, resupontos, resudensidade, resulpibper, resulsuperpoder;
 
     resupopulacao = populacao1 > populacao2;
-
-    printf("%d",resupopulacao);
+    resuarea = area1 > area2;
+    resupib = pib1 > pib2;
+    resupontos = pontos1 > pontos2;
+    resudensidade = densidade1 > densidade2;
+    resulpibper = pibcapital1 > pibcapital2;
+    resulsuperpoder = superpoder1 > superpoder2;
+    
+    printf("\nResultado Populacao:%d\n",resupopulacao);
+    printf("\nResultado Area:%d\n",resuarea);
+    printf("\nResultado PiB:%d\n",resupib);
+    printf("\nResultado Pontos turisticos:%d\n",resupontos);
+    printf("\nResultado Densidade:%d\n",resudensidade);
+    printf("\nResultado PiB Capital:%d\n",resulpibper);
+    printf("\nResultado Supor poder:%d\n",resulsuperpoder);
 
     return 0;
 }
